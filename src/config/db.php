b993ee90791b025c218df6773d46781d564a1d78
Sevi <correosevi@gmail.com>
2020-10-07 20:19:29 +0100

    :sparkles: Created the PDO DB driver

---
+++ src/config/db.php
@@ -0,0 +1,29 @@
+<?php
+namespace App\config;
+
+/* Main db class */
+class Conexion
+{
+    private $configuracion = [
+        'dsn' => 'mysql:host=localhost;dbname=db775411928;charset=utf8',
+        'usuario' => 'root',
+        'clave' => '',
+    ];
+
+    private $conexion = false;
+    public function obtenerPDO()
+    {
+        if ($this->conexion === false) {
+            $this->conexion = new \PDO(
+                $this->configuracion['dsn'],
+                $this->configuracion['usuario'],
+                $this->configuracion['clave']
+            );
+            $this->conexion->setAttribute(
+                \PDO::ATTR_ERRMODE,
+                \PDO::ERRMODE_EXCEPTION
+            );
+        }
+        return $this->conexion;
+    }
+}
