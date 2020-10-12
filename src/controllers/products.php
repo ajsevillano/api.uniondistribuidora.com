dc80ec9e29775d7c3c79f82d59222cc3436ed9bb
Sevi <correosevi@gmail.com>
2020-10-12 15:52:10 +0100

    :sparkles: Controller will return an error on item not found
    The controller now check if the response from the data base is empty
    when we ask for an ID and return and error in Json format explaining the
    item wasn't found
    
    This commit resolves #8

--- src/controllers/products.php
+++ src/controllers/products.php
@@ -23,6 +23,22 @@ class products
         $objetProductId = new productsRequest();
         $resultQueryId = $objetProductId->getId($arg['id']);
 
+        //Check if the response from the DB is empty and return an error message in this case.
+        if (empty($resultQueryId)) {
+            $emptyResult = json_encode(
+                [
+                    'status' => 'error',
+                    'Message' =>
+                        'The item ' .
+                        $arg['id'] .
+                        ' you requested do not exist',
+                ],
+                JSON_PRETTY_PRINT
+            );
+            $response->getBody()->write($emptyResult);
+            return $response->withHeader('Content-Type', 'application/json');
+        }
+        
         //Return the Product ID in an json object
         $encodeResult = json_encode($resultQueryId, JSON_PRETTY_PRINT);
         $response->getBody()->write($encodeResult);
