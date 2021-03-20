aaa198e5b46def94bcf337419a73aa87f926198c
Sevi <correosevi@gmail.com>
2021-03-20 14:16:47 +0000

    Ad auth to index

--- public/index.php
+++ public/index.php
@@ -1,4 +1,6 @@
 <?php
+<?php
 //Disable header in production
 //header("Access-Control-Allow-Origin: *");
+//header('Access-Control-Allow-Methods: GET, POST, PUT, PATCH, DELETE, HEAD, OPTIONS');
 require __DIR__ . "/../src/core/app.php";
